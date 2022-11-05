23d39f657105828031c2a677f579abf696dd3416
Bruno Cabral <cabral.brc@gmail.com>
2022-11-05 10:56:40 -0700

    euclidian distance

--- mnist_ml/KNN/src/knn.cc
+++ mnist_ml/KNN/src/knn.cc
@@ -29,6 +29,24 @@ void knn::find_knearest(data *query_point) {}
 void knn::set_k(int val) {}
 
 int knn::predict() {}
-double knn::calculate_distance(data *query_point, data *input) {}
+double knn::calculate_distance(data *query_point, data *input)
+{
+    double distance = 0.0;
+    if (query_point->get_feature_vector_size() != input->get_feature_vector_size())
+    {
+        printf("ERROR: vector size and input size mismatch\n");
+        exit(1);
+    }
+
+#ifdef EUCLID
+    for (unsigned i = 0; i < query_point->get_feature_vector_size(); i++)
+    {
+        distance += pow(query_point->get_feature_vector()->at(i) - input->get_feature_vector()->at(i), 2);
+    }
+    distance = sqrt(distance);
+#elif defined MANHATTAN
+#endif
+    return distance;
+}
 double knn::validate_performance() {}
 double knn::test_performance() {}
