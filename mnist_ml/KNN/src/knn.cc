66a28dcc86ebd9ba6af301a3b0bc5c157de52dbe
Bruno Cabral <cabral.brc@gmail.com>
2022-11-06 23:14:18 -0800

    main

--- mnist_ml/KNN/src/knn.cc
+++ mnist_ml/KNN/src/knn.cc
@@ -160,3 +160,38 @@ double knn::test_performance()
     printf("Test performance = %.3f %%\n", current_performance);
     return current_performance;
 }
+
+int main()
+{
+    data_handler *dh = new data_handler();
+    dh->read_feature_vector("../../MNIST/train-images.idx3-ubyte"); // for now, needs to come first
+    dh->read_feature_labels("../../MNIST/train-labels.idx1-ubyte");
+    dh->split_data();
+    dh->count_classes();
+    knn *knearest = new knn();
+    knearest->set_training_data(dh->get_training_data());
+    knearest->set_test_data(dh->get_test_data());
+    knearest->set_validation_data(dh->get_validation_data());
+    double performance = 0;
+    double best_performance = 0;
+    int best_k = 1;
+    for (int i = 1; i <= 4; i++)
+    {
+        knearest->set_k(i);
+        performance = knearest->validate_performance();
+        if (i == 1)
+        {
+            best_performance = performance;
+        }
+        else
+        {
+            if (performance > best_performance)
+            {
+                best_performance = performance;
+                best_k = i;
+            }
+        }
+    }
+    knearest->set_k(best_k);
+    knearest->test_performance();
+}
