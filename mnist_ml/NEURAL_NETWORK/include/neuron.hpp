94aa2124ab4b732e967d7476d37068c848df8444
Bruno Cabral <cabral.brc@gmail.com>
2022-11-08 13:07:49 -0800

    neuron class header

--- mnist_ml/NEURAL_NETWORK/include/neuron.hpp
+++ mnist_ml/NEURAL_NETWORK/include/neuron.hpp
@@ -1,5 +1,33 @@
 #pragma once
 
+#include <vector>
+#include <cmath>
+
 class Neuron
 {
+    std::vector<double> weights;
+    double preActivation;
+    double activatedOutput;
+    double outputDerivative;
+    double error;
+    double alpha; // used in activation functions
+
+public:
+    Neuron(int, int);
+    ~Neuron();
+    void initializeWeights(int previousLayerSize, int currentLayerSize);
+    void setError(double);
+    void setWeight(double, int);
+
+    double calculatePreActivation(std::vector<double>);
+    double calculateOutputDerivate();
+    double sigmoid();
+    double relu();
+    double leakyRelu();
+    double inverseSqrtRelu();
+
+    double getOutput();
+    double getOutputDerivative();
+    double getError();
+    std::vector<double> getWeights();
 };
