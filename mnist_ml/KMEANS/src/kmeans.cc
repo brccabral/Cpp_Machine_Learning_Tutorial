a7bd18d42abec3bd4dbdd5522cfa0df64abefc3e
Bruno Cabral <cabral.brc@gmail.com>
2022-11-08 10:05:10 -0800

    init_clusters_for_each_class

--- mnist_ml/KMEANS/src/kmeans.cc
+++ mnist_ml/KMEANS/src/kmeans.cc
@@ -9,21 +9,36 @@ kmeans::kmeans(int k)
 
 void kmeans::init_clusters()
 {
-    // create cluster for each label/class
+    // create K number of clusters
     for (int i = 0; i < num_clusters; i++)
     {
+        // pick a random point to be the centroid
         int index = (rand() % training_data->size()); // number between [0 and train.size-1]
-        // loop until find an unused label
+        // make sure we are not picking the same point
         while (used_indexes->find(index) != used_indexes->end())
         {
             index = (rand() % training_data->size());
         }
-        clusters->push_back(new cluster(training_data->at(index))); // create cluster with first point found for that label
-        used_indexes->insert(index); // set label as found
+        clusters->push_back(new cluster(training_data->at(index))); // create cluster with a random new point
+        used_indexes->insert(index);                                // save selected point
     }
 }
 
-void kmeans::init_clusters_for_each_class() {}
+void kmeans::init_clusters_for_each_class()
+{
+    // create cluster for each label/class
+    std::unordered_set<int> classes_used;
+    for (int i = 0; i < training_data->size(); i++)
+    {
+        // if new class is found, add to the list
+        if (classes_used.find(training_data->at(i)->get_label()) == classes_used.end())
+        {
+            clusters->push_back(new cluster_t(training_data->at(i)));
+            classes_used.insert(training_data->at(i)->get_label());
+            used_indexes->insert(i);
+        }
+    }
+}
 
 void kmeans::train() {}
 
