64f7bd82d56ead53939365ab9d2a66dbdc4e2ad3
Bruno Cabral <cabral.brc@gmail.com>
2022-11-07 21:44:03 -0800

    init_clusters

--- mnist_ml/KMEANS/src/kmeans.cc
+++ mnist_ml/KMEANS/src/kmeans.cc
@@ -1,12 +1,27 @@
 #include "kmeans.hpp"
 
-kmeans::kmeans(int k) {
+kmeans::kmeans(int k)
+{
     num_clusters = k;
     clusters = new std::vector<cluster_t *>;
     used_indexes = new std::unordered_set<int>;
 }
 
-void kmeans::init_clusters() {}
+void kmeans::init_clusters()
+{
+    // create cluster for each label/class
+    for (int i = 0; i < num_clusters; i++)
+    {
+        int index = (rand() % training_data->size()); // number between [0 and train.size-1]
+        // loop until find an unused label
+        while (used_indexes->find(index) != used_indexes->end())
+        {
+            index = (rand() % training_data->size());
+        }
+        clusters->push_back(new cluster(training_data->at(index))); // create cluster with first point found for that label
+        used_indexes->insert(index); // set label as found
+    }
+}
 
 void kmeans::init_clusters_for_each_class() {}
 
